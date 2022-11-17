b7a2e7a438ccbf574cf82e61094eca501f36c5f4
amannhr <amannhr129@gmail.com>
2022-11-17 11:04:16 +0530

    changes in app.js

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -19,8 +19,9 @@ const reviewRoutes = require('./routes/reviews');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 
+
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
-// 'mongodb://localhost:27017/yelp-camp'
+
 mongoose.connect(dbUrl); // Encode special character in password
 
 const db = mongoose.connection;
